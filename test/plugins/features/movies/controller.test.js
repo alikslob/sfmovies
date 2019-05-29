bf794c641fb5e04d8747e624a4f1ba275f424b80
alikslob <50843009+alikslob@users.noreply.github.com>
2019-05-28 17:23:03 -0700

    feat(get): creates GET /movies endpoint (#9)
    * test(validators): added unit tesst for the movie validator
    
    * feat(controllers): added controller for post /movies end point
    
    * test(controller): adds unit tests for controller for movies
    
    * test(integration): adds integration test for movies
    
    * feat(get): creates GET /movies endpoint

--- test/plugins/features/movies/controller.test.js
+++ test/plugins/features/movies/controller.test.js
@@ -8,10 +8,46 @@ describe('movie controller', () => {
 
     it('creates a movie', async () => {
       const payload = { title: 'WALL-E' };
-
       const movie = await Controller.create(payload);
-
       expect(movie.get('title')).to.eql(payload.title);
+
+    });
+
+  });
+
+  describe('list', () => {
+
+    it('returns an array of movies', async () => {
+      const query = {};
+
+      const movies = await Controller.find(query);
+
+      expect(movies).to.be.an('object');
+    });
+
+    it('returns an array of movies released at an exact year', async () => {
+      const query = { release_year: 1999 };
+
+      const movies = await Controller.find(query);
+      const initialLength = movies.length;
+      movies.filter((movie) => movie.release_year = 1999);
+      const filteredLength = movies.length;
+
+      expect(filteredLength).to.eql(initialLength);
+    });
+
+    it('returns an array of movies in range of years', async () => {
+      const query = {
+        start_year: 1999,
+        end_year: 2010
+      };
+
+      const movies = await Controller.find(query);
+      const initialLength = movies.length;
+      movies.filter((movie) => movie.release_year >= 1999 && movie.release_year <= 2010);
+      const filteredLength = movies.length;
+
+      expect(filteredLength).to.eql(initialLength);
     });
 
   });
