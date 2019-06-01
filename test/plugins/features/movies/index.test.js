88bc587b50baaa650eaf12e310669a5c87968043
alikslob <50843009+alikslob@users.noreply.github.com>
2019-05-31 17:46:10 -0700

    feat(locations): associate movies with multiple locations (#11)

--- test/plugins/features/movies/index.test.js
+++ test/plugins/features/movies/index.test.js
@@ -20,4 +20,22 @@ describe('movies integration', () => {
 
   });
 
+  describe('add', () => {
+
+    it('adds a new location to a movie', () => {
+      return Movies.inject({
+        url: '/movies/1/location',
+        method: 'POST',
+        payload: { name: 'Soma' }
+      })
+      .then((response) => {
+        expect(response.statusCode).to.eql(200);
+        expect(response.result.object).to.eql('location_movie');
+        expect(response.result.movie_id).to.eql(1);
+        expect(response.result.location_id).to.eql(1);
+      });
+    });
+
+  });
+
 });
